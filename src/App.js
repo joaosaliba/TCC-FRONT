6ff6c0d7f7f29798c671b4d02b64fab107d10442
Joao RIbeiro <joao.ribeiro@hepta.com.br>
2021-09-30 17:03:45 -0300

    exemplo requisição axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  async componentDidMount() {
+    try {
+      axios.post('http://0.0.0.0:8040/Aluno/',{
+        nome: "CAIO GABRIEL ARAUJO MEDEIROS",
+        email: "caio@gmail.com",
+        birthdate: "1996-12-22",
+        phonenumber: "+5561987475896",
+        // picture: "./public/logo192.png",
+        user_type: "Aluno",
+        cpf: "56480946120",
+        password:"33525719J",
+        password_confirmation:"33525719J",
+        
+    })
+      .then(res=>{
+        console.log(res)
+      }).catch(e=>{
+        console.error(e.response.data)
+      });
+      
+      
+    } catch (e) {
+      console.log(e);
+  }
+
+  }
+  render(){
+    return <h1> oi</h1>
+  }
 }
 
 export default App;
