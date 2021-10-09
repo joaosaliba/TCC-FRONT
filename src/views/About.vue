907590c89bfa16b18c550c59d6f10a80f1c828b9
Joao RIbeiro <joao.ribeiro@hepta.com.br>
2021-10-08 22:52:43 -0300

    Alterando de React para VueJS, com bootstrap, router e axios

---
+++ src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div >
+    <b-row class="align-items-center">
+      <b-col class="text-right">
+        <h4>Dados Nota</h4>
+      </b-col>
+      <b-col class="text-left">
+        <h4>Dados Nota</h4>
+      </b-col>
+    </b-row>
+     <b-row class="align-items-center">
+      <b-col class="text-left">
+        <h4>Dados </h4>
+      </b-col>
+    </b-row>
+  </div>
+</template>
