3dcf3301baeb2df1e9bbf1b4a1fbfdb63da4b1c5
Joao RIbeiro <joao.ribeiro@hepta.com.br>
2021-10-13 22:24:41 -0300

    criando funcoes necessarias para requisiçao e interceptacao e adicao de token

---
+++ src/utils/api.js
@@ -0,0 +1,46 @@
+import Vue from 'vue'
+
+import axios from 'axios'
+
+const axiosInstance = axios.create({
+    baseURL: "http://localhost:8040/",
+    // withCredentials: true
+})
+
+axiosInstance.interceptors.request.use(
+    function(config){
+        const token = window.localStorage.token
+        if(token){
+            config.headers.Authorization = token
+        }
+        return config
+    },
+    function erro(erro){
+        return Promise.reject(erro);
+    }
+)
+
+export const api={
+    get(endpoint){
+        return axiosInstance.get(endpoint)
+    },
+    post(endpoint,body){
+        return axiosInstance.post(endpoint,body)
+    },
+    put(endpoint,body){
+        return axiosInstance.put(endpoint,body)
+    },
+    delete(endpoint){
+        return axiosInstance.post(endpoint)
+    },
+    login(body){
+        return axiosInstance.post("api-token-auth/",body)
+    }
+
+}
+
+Vue.use({
+    install(Vue) {
+      Vue.prototype.$api = api;
+    }
+  });
---
+++ src/utils/store.js
@@ -0,0 +1,60 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import { api } from './api'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state:{
+        login: false,
+        usuario:{
+            id:"",
+            nome:"",
+            email:"",
+            birthdate:"",
+            phonenumber:"",
+            picture:"",
+            user_type:"",
+            cpg:"",           
+
+        }
+    },
+    mutations:{
+        UPDATE_LOGIN(state,payload){
+            state.login =payload
+        },
+        UPDATE_USUARIO(state,payload){
+            state.usuario =payload
+        }
+    },
+    actions:{
+        getUsuario(context){
+            
+            api.get("Professor/").then(resp=>{
+                context.commit("UPDATE_LOGIN",true)
+                context.commit("UPDATE_USUARIO",resp.data)
+                console.log(this.state.usuario)
+            }).catch(r=>{
+                api.get("Aluno/").then(resp=>{
+                    context.commit("UPDATE_LOGIN",true)
+                    context.commit("UPDATE_USUARIO",resp.data)
+                    console.log(this.state.usuario)
+                })
+
+            })
+        
+        },
+     logarUsuario(context,payload){
+         return api.login({
+            email:  payload.email,
+            password: payload.password,
+            device_type:"ANDROID",
+            device_id:"device-id-ficticio"
+         }).then(response=>{
+            window.localStorage.token =`JWT ${response.data.token}`
+            console.log( window.localStorage.token)
+         })
+     }
+    },
+
+})
