07fc0528bc6ea0b5e43d1d02a3ec0658d335e8a7
Joao RIbeiro <joao.ribeiro@hepta.com.br>
2021-10-15 19:30:38 -0300

    botoes rede social e estilizaçoes tela login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,54 +1,65 @@
 <template>
   <div>
     <div back-rede>
-    <img alt="logo " class="logo" src="../assets/img/logo.svg">
-    
-       <div id="autentica">
+      <img alt="logo " class="logo" src="../assets/img/logo.svg" />
 
-          <div class="mb-1">
-           <label for="login">
-             <b>Login</b>
-           </label>
-            <input
-              type="text"
-              class="form-control"
-              name
-              id="login"
-              aria-describedby="usuario"
-              placeholder="Seu login"
-              v-model="usuario.email"
-            />
-            <div class="mb-2"></div>
-                 <label for="senha">
-             <b> Senha</b>
-           </label>
-            <input
-              type="password"
-              class="form-control"
-              name
-              id="senha"
-              aria-describedby="senha"
-              placeholder="Sua senha"
-              v-model="usuario.password"
-              @keyup.enter="login"
-            />
-            <div class="mt-1" >
-              <button
-                type="button"
-                class="btn"
-                @click.prevent="login()"
-              >
-                Entrar
-              </button>
-            </div>
+      <form>
+        <div class="mb-1">
+          <label for="login">
+            <b>Login</b>
+          </label>
+          <input
+            type="email"
+            class="form-control input-round"
+            id="login"
+            aria-describedby="usuario"
+            placeholder="Seu login"
+            v-model="usuario.email"
+          />
+          <div class="mb-2"></div>
+          <label for="senha">
+            <b> Senha</b>
+          </label>
+          <input
+            type="password"
+            class="form-control input-round"
+            id="senha"
+            aria-describedby="senha"
+            placeholder="Sua senha"
+            v-model="usuario.password"
+            @keyup.enter="login"
+          />
+          <div class="mt-1">
+            <router-link to="/login">
+              Esqueceu senha ou Login?
+            </router-link>
+            <button type="button" class="btn" @click.prevent="login()">
+              Entrar
+            </button>
           </div>
         </div>
+        <b-button class="redes">
+            <img
+              alt="Google "
+              class="redesImagens"
+              src="../assets/img/googleLogo.png"
+            />Entrar com Google
+        </b-button>
+        <br>
+         <b-button class="redes">
+            <img
+              alt="Google "
+              class="redesImagens"
+              src="../assets/img/logo-facebook.png"
+            />
+            Entrar com Facebook
+        </b-button>
+      </form>
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
   name: "Login",
   data() {
@@ -57,56 +68,90 @@ export default {
       usuario: {
         email: "",
         password: "",
-        device_type:"ANDROID",
-        device_id:"device-id-ficticio"
+        device_type: "ANDROID",
+        device_id: "device-id-ficticio",
       },
     };
   },
-  methods:{
-    login(){
-      this.$store.dispatch("logarUsuario",this.usuario).then(response=>{
-         this.$store.dispatch("getUsuario")
-         this.$router.push({
-           path: '/home',
-         })
-      })
-    }
-  }
+  methods: {
+    login() {
+      this.$store.dispatch("logarUsuario", this.usuario).then((response) => {
+        this.$store.dispatch("getUsuario");
+        this.$router.push({
+          path: "/home",
+        });
+      });
+    },
+  },
 };
 </script>
 
 <style scoped>
-#autentica {
-  font-size: 16px;
+.input-round {
+  border-radius: 15px;
+  height: 50px;
+  background-color: #ffffff;
+  box-shadow: 0 3px 6px #00000029;
+}
+form {
+  display: grid;
+  font-size: 30x;
   padding: 20px 40px 20px 40px;
   background-color: #fff;
-  width: 500px;
-  text-align: left;
-  margin: 0 auto;
-  margin-top: 1%;
   border-radius: 50px;
   opacity: 0.79;
-  
-  
+  max-width: 500px;
+  width: auto;
+  margin: 0 auto;
+  margin-top: 1%;
 }
-.logo{
+a {
+  list-style: none;
+  font-size: 30x;
+  margin-top: 1%;
+  color: #0b4f6c;
+}
+.logo {
+  margin: auto auto;
   margin-top: 5%;
-  margin-left: 42%;
-  text-align: center;
   position: flex;
 }
 
-.btn{
-    display: block;
-    padding: 10px 30px;
-    background-color: #0B4F6C;
-    border-radius: 4px;
-    color: white;
-    text-align: right;
-    font-size: 1rem;
-    /* box-shadow: 0 4px 8px #00000029; */
+.btn {
+  display: block;
+  padding: 10px 30px;
+  background-color: #0b4f6c;
+  border-radius: 25px;
+  color: white;
+  text-align: right;
+  font-size: 1rem;
+  margin: 0 auto;
+  margin-right: 0%;
+  /* margin-top: 5%; */
+  /* box-shadow: 0 4px 8px #00000029; */
 }
-.btn:hover{
-  transform: scale(1.1);
+
+.btn:hover {
+  transform: scale(1.05);
+}
+.redes {
+  padding: 10px 30px;
+
+  display: table-cell;
+  margin: auto auto;
+  background-color: #ffffff;
+  color: #0b4f6c;
+  width: 400px;
+  text-align: center;
+  font: 25px Roboto Black;
+  border-color: transparent;
+  box-shadow: 0px 3px 6px #00000029;
+  opacity: 1 !important;
+  
+}
+.redesImagens {
+  display: table;
+  float: left;
+  width: 50px;
 }
 </style>
